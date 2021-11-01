bc698d3fa3db88ccde936a84a716f43f5123d1ee
bimashazaman <bimashazaman1234@gmail.com>
2021-11-01 17:21:59 +0600

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener('click', function () {
-  console.log('button clicked add event')
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += '<li>' + myLeads[i] + '</li>'
+}
